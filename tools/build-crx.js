c7fde8b4568e68b6a0f5e3f4a1759082b73320eb
eight04 <eight04@gmail.com>
2025-11-28 22:52:58 +0800

    Fix: extension name

--- tools/build-crx.js
+++ tools/build-crx.js
@@ -1,6 +1,6 @@
 import {$} from 'execa';
 import glob from 'tiny-glob';
 
-const zipPath = (await glob('web-ext-artifacts/bookmark_sync-*.zip')).pop();
+const zipPath = (await glob('web-ext-artifacts/bookmark*.zip')).pop();
 
 await $({stdin: {file: zipPath}})`crx3 -p key.pem -o ${zipPath.replace('.zip', '.crx')}`;
